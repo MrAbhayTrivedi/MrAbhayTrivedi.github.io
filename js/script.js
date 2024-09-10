fff221f87cb17f8ce3b0fe7f25a79745ca6f71d2
Abhay Trivedi <abhaytrivedi838@gmail.com>
2024-09-11 01:40:52 +0530

    updated js file

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`https://github.com/MrAbhayTrivedi/MrAbhayTrivedi.github.io/tree/main/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -74,7 +74,9 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    song_folder = 'songs'
+    // console.log('Inside displayAlbums')
+    let a = await fetch(`/${song_folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -86,7 +88,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
+            let a = await fetch(`https://github.com/MrAbhayTrivedi/MrAbhayTrivedi.github.io/tree/main/${song_folder}/${folder}/info.json`)
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
@@ -97,7 +99,7 @@ async function displayAlbums() {
                 </svg>
             </div>
 
-            <img src="/songs/${folder}/cover.jpg" alt="">
+            <img src="https://github.com/MrAbhayTrivedi/MrAbhayTrivedi.github.io/tree/main/${song_folder}/${folder}/cover.jpg" alt="">
             <h2>${response.title}</h2>
             <p>${response.description}</p>
         </div>`
@@ -117,7 +119,7 @@ async function displayAlbums() {
 
 async function main() {
     // Get the list of all the songs
-    await getSongs("songs/ncs")
+    await getSongs("songs/Chill_(mood)")
     playMusic(songs[0], true)
 
     // Display all the albums on the page
